f743ac0d50a4aabbcc9ae2f908c61b937a9ade44
Michael <michael@n90x.info>
2025-05-23 11:01:02 +0200

    Add the needed page to list of all the games that are in our inventory.

--- Boardium/Boardium/Controllers/GamesController.cs
+++ Boardium/Boardium/Controllers/GamesController.cs
@@ -1,13 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Boardium.Models;
+using Boardium.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Boardium.Controllers {
     public class GamesController : Controller {
-        public IActionResult Index() {
+        private readonly BoardiumContext _context;
+        private readonly ILogger<GamesController> _logger;
+
+        public GamesController(BoardiumContext context, ILogger<GamesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public IActionResult BoardGames() {
             return View();
         }
 
-        public IActionResult BoardGame(int id) {
-            return View();
+        public async Task<IActionResult> Index(int? page) {
+            int pageSize = 10;
+            int currentPage = page ?? 1;
+            List<Models.Game.Game> games = await _context.Games
+                                                         .OrderBy(g => g.Id)
+                                                         .Skip((currentPage - 1) * pageSize)
+                                                         .Take(pageSize)
+                                                         .ToListAsync();
+            int totalGames = await _context.Games
+                                           .CountAsync();
+
+
+            BoardGameViewModel model = new BoardGameViewModel {
+                CurrentPage = currentPage,
+                HasPreviousPage = currentPage > 1,
+                HasNextPage = currentPage * pageSize < totalGames,
+                Games = games.Select(g => new BoardGame {
+                    Title = g.Title,
+                    Publisher = "PublisherName",
+                    Description = g.Description,
+                    PathToImage = "ImagePath"
+                }).ToList()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ Boardium/Boardium/Models/BoardGameViewModel.cs
@@ -0,0 +1,17 @@
+﻿// This is for the Games Controller to load the pages with all the board games that we have.
+// THIS IS NOT PART OF THE DATABASE!
+namespace Boardium.Models {
+    public class BoardGameViewModel {
+        public int CurrentPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+        public bool HasNextPage { get; set; }
+        public List<BoardGame> Games { get; set; }
+    }
+
+    public class BoardGame {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Description { get; set; }
+        public string PathToImage { get; set; }
+    }
+}
