ce420da079bfafd59e70728fe040b2fdf8cbb9fc
Michael <michael@n90x.info>
2025-05-27 22:08:13 +0200

    Prepare to show basic information about the game, fees, etc. when the user wants to rent it.

--- Boardium/Boardium/Controllers/RentalsController.cs
+++ Boardium/Boardium/Controllers/RentalsController.cs
@@ -8,24 +8,56 @@ namespace Boardium.Controllers {
     public class RentalsController : Controller {
         private readonly BoardiumContext _context;
         private readonly ILogger<GamesController> _logger;
+        public RentalsController(BoardiumContext context, ILogger<GamesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
         
         [Authorize(Roles = "Admin,Employee,User")]
         public async Task<IActionResult> Index(int GameID, int GameCopyID) {
-            GameAvailableCopy[] gameCopies = await (from gc in _context.GameCopies
-                                                    join r in _context.Rentals on gc.Id equals r.GameCopyId into rentalsGroup
-                                                    from rental in rentalsGroup.DefaultIfEmpty()
-                                                    where gc.GameId == GameID && gc.Id == GameCopyID
-                                                    select new GameAvailableCopy {
-                                                        GameCopyID = gc.Id,
-                                                        GameID = gc.GameId,
-                                                        Condition = gc.Condition,
-                                                        InventoryNumber = gc.InventoryNumber,
-                                                        RentalFee = gc.RentalFee,
-                                                        DueDate = rental.DueDate
-                                                    }
-                                                   ).ToArrayAsync();
+            GameAvailableCopyDetailsViewModel? gameCopyDetail =
+                await (from gc in _context.GameCopies
+                       join g in _context.Games on gc.GameId equals g.Id
+                       join r in _context.Rentals on gc.Id equals r.GameCopyId into rentalGroup
+                       from rental in rentalGroup.DefaultIfEmpty() // LEFT JOIN
+                       where gc.GameId == 1 && gc.Id == 1
+                       select new GameAvailableCopyDetailsViewModel {
+                                                                     GameCopyID = gc.Id,
+                                                                       GameID = gc.GameId,
+                                                                       Title = g.Title,
+                                                                       Condition = gc.Condition,
+                                                                       InventoryNumber = gc.InventoryNumber,
+                                                                       RentalFee = gc.RentalFee,
+                                                                       DueDate = rental.DueDate,
+                                                                       LateFee = 0.0m,
+                                                                       DamageFee = 0.0m,
+                                                                       FutureBorrows = new List<BorrowInfo>(),
+                                                                       PathToImage = "" 
+                       }).FirstOrDefaultAsync();
+            // await (from gc in _context.GameCopies
+            //                                                       join r in _context.Rentals on gc.Id equals r.GameCopyId into rentalsGroup
+            //                                                       from rental in rentalsGroup.DefaultIfEmpty()
+            //                                                       join g in _context.Games on gc.GameId equals g.Id 
+            //                                                       where gc.GameId == GameID && gc.Id == GameCopyID
+            //                                                       select new GameAvailableCopyDetailsViewModel {
+            //                                                           GameCopyID = gc.Id,
+            //                                                           GameID = gc.GameId,
+            //                                                           Title = g.Title,
+            //                                                           Condition = gc.Condition,
+            //                                                           InventoryNumber = gc.InventoryNumber,
+            //                                                           RentalFee = gc.RentalFee,
+            //                                                           DueDate = rental.DueDate,
+            //                                                           LateFee = 0.0m,
+            //                                                           DamageFee = 0.0m,
+            //                                                           FutureBorrows = null,
+            //                                                           PathToImage = "" 
+            //                                                       }
+            //                                                      ).FirstOrDefaultAsync();
+            if (gameCopyDetail == null)
+                return NotFound();
 
-            return View();
+            return View(gameCopyDetail);
         }
     }
 }
---
+++ Boardium/Boardium/Models/GameAvailableCopyDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Boardium.Models.Inventory;
+
+namespace Boardium.Models {
+
+    public class GameAvailableCopyDetailsViewModel {
+        public int GameCopyID { get; set; }
+        public int GameID { get; set; }
+        public string? Title {  get; set; }
+        public string InventoryNumber { get; set; }
+        public GameCondition Condition { get; set; }
+        public decimal RentalFee { get; set; }
+        public decimal LateFee { get; set; }
+        public decimal DamageFee { get; set; }
+        public DateTime? DueDate { get; set; }
+        public List<BorrowInfo>? FutureBorrows { get; set; }
+        public string PathToImage { get; set; }
+    }
+
+    public class BorrowInfo {
+        public DateTime RentDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
