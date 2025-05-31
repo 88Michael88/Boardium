8ae4942e3cb1ce362ec71b2818ec550efd71b030
Michael <michael@n90x.info>
2025-05-31 12:20:32 +0200

    Prepare the test solution for tests.

--- Boardium/BasicUnitTests/HelperFunction_Tester.cs
+++ Boardium/BasicUnitTests/HelperFunction_Tester.cs
@@ -1,8 +1,23 @@
-﻿namespace BasicUnitTests {
-    public class RentalsController_Tests {
+﻿using Boardium.HelperFuncs;
+using Boardium.Models;
+
+namespace BasicUnitTests {
+    public class HelperFunctions {
+        BorrowInfo[] _borrowInfo;
+        [SetUp]
+        public void Setup() {
+            BorrowInfo firstBI = new BorrowInfo();
+            firstBI.BorrowDate = DateTime.Now.AddDays(-2);
+            firstBI.DueDate = DateTime.Now.AddDays(5);
+            BorrowInfo secondBI = new BorrowInfo();
+            BorrowInfo thirdBI = new BorrowInfo();
+            _borrowInfo[0] = firstBI;
+            _borrowInfo[1] = secondBI;
+            _borrowInfo[2] = thirdBI;
+        }
 
         [Test]
-        public void Test1() {
+        public void DateIsBetweenDates() {
             Assert.Pass();
         }
     }
