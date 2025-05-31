a5a426fe3bef0bca4504716c96afa5f2533818b2
Michael <michael@n90x.info>
2025-05-31 19:11:39 +0200

    Set up for Unit Tests.

--- Boardium/BasicUnitTests/HelperFunction_Tester.cs
+++ Boardium/BasicUnitTests/HelperFunction_Tester.cs
@@ -3,14 +3,18 @@ using Boardium.Models;
 
 namespace BasicUnitTests {
     public class HelperFunctions {
-        BorrowInfo[] _borrowInfo;
+        BorrowInfo[] _borrowInfo = new BorrowInfo[3];
         [SetUp]
         public void Setup() {
             BorrowInfo firstBI = new BorrowInfo();
             firstBI.BorrowDate = DateTime.Now.AddDays(-2);
             firstBI.DueDate = DateTime.Now.AddDays(5);
             BorrowInfo secondBI = new BorrowInfo();
+            secondBI.BorrowDate = DateTime.Now.AddDays(7);
+            secondBI.DueDate = DateTime.Now.AddDays(12);
             BorrowInfo thirdBI = new BorrowInfo();
+            thirdBI.BorrowDate = DateTime.Now.AddDays(13);
+            thirdBI.DueDate = DateTime.Now.AddDays(18);
             _borrowInfo[0] = firstBI;
             _borrowInfo[1] = secondBI;
             _borrowInfo[2] = thirdBI;
@@ -18,7 +22,7 @@ namespace BasicUnitTests {
 
         [Test]
         public void DateIsBetweenDates() {
-            Assert.Pass();
+
         }
     }
 }
